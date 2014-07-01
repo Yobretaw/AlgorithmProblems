b413761ba8a87f63547d6d049a6420d8e525262f
Yobretaw <youkeshen@gmail.com>
2014-07-01 00:45:28 -0400

    reverseAlternateLevelOfAPerfectBST.cpp

--- Tree/BSTStructure.h
+++ Tree/BSTStructure.h
@@ -1,21 +1,122 @@
+#include <fstream>
+#include <iostream>
+#include <deque>
+#include <iomanip>
+#include <sstream>
+#include <string>
+#include <cmath>
+using namespace std;
+
 struct node {
-	int val;
-	node *left;
-	node *right;
+  node *left, *right;
+  int val;
+  node(int val) : left(NULL), right(NULL), val(val) { }
+  node(int val, node* left, node* right): left(left), right(right), val(val) {  }
 };
 
-node *makeBST(int val, node *left, node *right) {
-	node *ret = new node;
-	ret->val = val;
-	ret->left = left;
-	ret->right = right;
-	return ret;
+// Find the maximum height of the binary tree
+int maxHeight(node *p) {
+  if (!p) return 0;
+  int leftHeight = maxHeight(p->left);
+  int rightHeight = maxHeight(p->right);
+  return (leftHeight > rightHeight) ? leftHeight + 1: rightHeight + 1;
 }
 
-void printBST(node *root) {
-	if(root == NULL)
-		return;
-	printBST(root->left);
-	std::cout << root->val << " ";
-	printBST(root->right);
+// Convert an integer value to string
+string intToString(int val) {
+  ostringstream ss;
+  ss << val;
+  return ss.str();
+}
+
+// Print the arm branches (eg, /    \ ) on a line
+void printBranches(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<node*>& nodesQueue, ostream& out) {
+  deque<node*>::const_iterator iter = nodesQueue.begin();
+  for (int i = 0; i < nodesInThisLevel / 2; i++) {  
+    out << ((i == 0) ? setw(startLen-1) : setw(nodeSpaceLen-2)) << "" << ((*iter++) ? "/" : " ");
+    out << setw(2*branchLen+2) << "" << ((*iter++) ? "\\" : " ");
+  }
+  out << endl;
+}
+
+// Print the branches and node (eg, ___10___ )
+void printNodes(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<node*>& nodesQueue, ostream& out) {
+  deque<node*>::const_iterator iter = nodesQueue.begin();
+  for (int i = 0; i < nodesInThisLevel; i++, iter++) {
+    out << ((i == 0) ? setw(startLen) : setw(nodeSpaceLen)) << "" << ((*iter && (*iter)->left) ? setfill('_') : setfill(' '));
+    out << setw(branchLen+2) << ((*iter) ? intToString((*iter)->val) : "");
+    out << ((*iter && (*iter)->right) ? setfill('_') : setfill(' ')) << setw(branchLen) << "" << setfill(' ');
+  }
+  out << endl;
+}
+
+// Print the leaves only (just for the bottom row)
+void printLeaves(int indentSpace, int level, int nodesInThisLevel, const deque<node*>& nodesQueue, ostream& out) {
+  deque<node*>::const_iterator iter = nodesQueue.begin();
+  for (int i = 0; i < nodesInThisLevel; i++, iter++) {
+    out << ((i == 0) ? setw(indentSpace+2) : setw(2*level+2)) << ((*iter) ? intToString((*iter)->val) : "");
+  }
+  out << endl;
+}
+
+// Pretty formatting of a binary tree to the output stream
+// @ param
+// level  Control how wide you want the tree to sparse (eg, level 1 has the minimum space between nodes, while level 2 has a larger space between nodes)
+// indentSpace  Change this to add some indent space to the left (eg, indentSpace of 0 means the lowest level of the left node will stick to the left margin)
+void printPretty(node *root, int level, int indentSpace, ostream& out) {
+  int h = maxHeight(root);
+  int nodesInThisLevel = 1;
+
+  int branchLen = 2*((int)pow(2.0,h)-1) - (3-level)*(int)pow(2.0,h-1);  // eq of the length of branch for each node of each level
+  int nodeSpaceLen = 2 + (level+1)*(int)pow(2.0,h);  // distance between left neighbor node's right arm and right neighbor node's left arm
+  int startLen = branchLen + (3-level) + indentSpace;  // starting space to the first node to print of each level (for the left most node of each level only)
+    
+  deque<node*> nodesQueue;
+  nodesQueue.push_back(root);
+  for (int r = 1; r < h; r++) {
+    printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+    branchLen = branchLen/2 - 1;
+    nodeSpaceLen = nodeSpaceLen/2 + 1;
+    startLen = branchLen + (3-level) + indentSpace;
+    printNodes(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+
+    for (int i = 0; i < nodesInThisLevel; i++) {
+      node *currNode = nodesQueue.front();
+      nodesQueue.pop_front();
+      if (currNode) {
+	      nodesQueue.push_back(currNode->left);
+	      nodesQueue.push_back(currNode->right);
+      } else {
+        nodesQueue.push_back(NULL);
+        nodesQueue.push_back(NULL);
+      }
+    }
+    nodesInThisLevel *= 2;
+  }
+  printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+  printLeaves(indentSpace, level, nodesInThisLevel, nodesQueue, out);
+}
+
+//int main() {
+  //node *root = new node(30);
+  //root->left = new node(20);
+  //root->right = new node(40);
+  //root->left->left = new node(10);
+  //root->left->right = new node(25);
+  //root->right->left = new node(35);
+  //root->right->right = new node(50);
+  //root->left->left->left = new node(5);
+  //root->left->left->right = new node(15);
+  //root->left->right->right = new node(28);
+  //root->right->right->left = new node(41);
+
+  //cout << "Tree pretty print with level=1 and indentSpace=0\n\n";
+  //// Output to console
+  //printPretty(root, 1, 0, cout);
+
+  //return 0;
+//}
+
+void printBST(node* root) {
+  printPretty(root, 1, 0, cout);
 }
---
+++ Tree/reverseAlternateLevelsOfAPerfectBST.cpp
@@ -0,0 +1,56 @@
+#include "iostream"
+#include "vector"
+#include "stack"
+#include "map"
+#include "BSTStructure.h"
+using namespace std;
+
+void reverse(node* root, vector<int>& v, int level, bool isFirst);
+
+int main()
+{
+  map<int, stack<int> > m;
+  vector<int> v;
+  node* root = new node(7, 
+      new node(3, 
+        new node(1, 
+          new node(0, NULL, NULL),
+          new node(2, NULL, NULL)),
+        new node(5,
+          new node(4, NULL, NULL),
+          new node(6, NULL, NULL))),
+      new node(11,
+        new node(9,
+          new node(8, NULL, NULL),
+          new node(10, NULL, NULL)),
+        new node(13,
+          new node(12, NULL, NULL),
+          new node(14, NULL, NULL))));
+
+  printBST(root);
+  reverse(root, v, 0, true);
+  reverse(root, v, 0, false);
+  printBST(root);
+  return 0;
+}
+
+void reverse(node* root, vector<int>& v, int level, bool isFirst) {
+  if(root == NULL) return;
+
+  if(level % 2 == 0) {
+    reverse(root->left, v, level+1, isFirst);
+    reverse(root->right, v, level+1, isFirst);
+    return;
+  }
+
+  // inorder traversal
+  reverse(root->left, v, level+1, isFirst);
+  if(isFirst) {
+    v.push_back(root->val);
+  } else {
+    root->val = v.back();
+    v.pop_back();
+  }
+  reverse(root->right, v, level+1, isFirst);
+  return;
+}
