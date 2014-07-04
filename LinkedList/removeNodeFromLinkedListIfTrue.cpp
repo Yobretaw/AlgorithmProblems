135063469fc2387f94fa2f8f0608b93eb21e4627
Y+SHEN <Yobretaw@users.noreply.github.com>
2014-07-04 15:30:22 -0400

    Update removeNodeFromLinkedListIfTrue.cpp

--- LinkedList/removeNodeFromLinkedListIfTrue.cpp
+++ LinkedList/removeNodeFromLinkedListIfTrue.cpp
@@ -1,1 +1,22 @@
-#include <
+#include <iostream>
+#include <linkedListStructure.h>
+using namespace std;
+
+void removeOdd(node*& head) {
+  for(node** curr = &head; *curr; ) {
+    node* entry = *curr;
+    if(curr->val % 2 == 1) {
+      *curr = entry->next;
+      delete entry;
+    } else {
+      *curr = &(entry->next);
+    }
+  }
+}
+
+int main() {
+  node* a = makenode(1, makenode(2, makenode(3, makenode(4, makenode(5, makenode(6, NULL))))));
+  removeOdd(a);
+  printNode(a);
+  deleteNode(a);
+}
