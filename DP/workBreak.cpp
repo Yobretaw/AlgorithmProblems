d2bcd4f209ef6abb0726b17a3fba9e45e73c1216
Yobretaw <youkeshen@gmail.com>
2014-07-13 15:09:02 -0400

    wordbreak.cpp

--- DP/workBreak.cpp
+++ DP/workBreak.cpp
@@ -3,6 +3,12 @@
 #include "vector"
 using namespace std;
 
+// Given a string s and a dictionary of words dict, determine if s can be segmented into a space-separated sequence of one or more dictionary words.
+// For example, given
+// s = "leetcode",
+// dict = ["leet", "code"].
+// Return true because "leetcode" can be segmented as "leet code".
+
 bool wordBreak(string s, vector<string> dict) {
   bool* t = new bool(s.length() + 1);
   t[0] = true;
