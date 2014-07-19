307cde6d22c60c4e760203b5b8a070b614c0763b
Yobretaw <youkeshen@gmail.com>
2014-07-19 14:37:12 -0400

    removeDuplicatesWithoutBuffer.cpp

---
+++ LinkedList/removeDuplicatedsWithoutBuffer.cpp
@@ -0,0 +1,35 @@
+#include "iostream"
+#include "linkedListStructure.h"
+using namespace std;
+
+void remove(node* head) {
+  if(head == NULL) {
+    return;
+  }
+
+  while(head) {
+    int val = head->val;
+    node* pre = head;
+    node* curr = head->next;
+
+    while(curr) {
+      if(curr->val == val) {
+        pre->next = curr->next;
+        delete curr;
+        curr = pre->next;
+      } else {
+        pre = curr;
+        curr = curr->next;
+      }
+    }
+    head = head->next;
+  }
+}
+
+int main()
+{
+  node* head = new node(1, new node(2, new node(1, new node(3, new node(3, new node(4, new node(4, NULL)))))));
+  remove(head);
+  printNode(head);
+  return 0;
+}
