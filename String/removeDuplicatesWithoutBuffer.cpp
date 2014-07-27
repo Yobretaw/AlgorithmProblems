d2eaee8a50bdaefbd60f006d9cf66fe9e2182962
Yobretaw <youkeshen@gmail.com>
2014-07-27 13:21:41 -0400

    removeDuplicatesWithoutBuffer.cpp

---
+++ String/removeDuplicatesWithoutBuffer.cpp
@@ -0,0 +1,37 @@
+#include "iostream"
+#include "string"
+using namespace std;
+
+void remove(string& s) {
+  int len = s.length();
+
+  if(len < 2)
+    return;
+
+  int tail = 1;
+  for(int i = 1; i < len; i++) {
+    int j;
+    for(j = 0; j < tail; j++) {
+      if(s[i] == s[j]) {
+        break;
+      }
+    }
+
+    if(j == tail) {
+      s[tail] = s[i];
+      tail++;
+    }
+  }
+
+  s[tail] = '\0';
+}
+
+int main()
+{
+  string s = "abebabcxbabebebabbbcabbebebabbs";
+  remove(s);
+  cout << s << endl;
+  return 0;
+}
+
+
