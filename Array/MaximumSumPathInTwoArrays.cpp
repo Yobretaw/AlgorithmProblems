4d1a5ba3761467bf784b74e7d31125de0bf0fae5
Y+S <Yobretaw@users.noreply.github.com>
2014-09-17 17:37:49 -0400

    Update MaximumSumPathInTwoArrays.cpp

--- Array/MaximumSumPathInTwoArrays.cpp
+++ Array/MaximumSumPathInTwoArrays.cpp
@@ -1,6 +1,33 @@
 #include<iostream>
 using namespace std;
- 
+
+/*
+Given two sorted arrays such the arrays may have some common elements. Find the sum of the maximum sum path to reach from beginning of any array to end of any of the two arrays. We can switch from one array to another array only at common elements.
+
+Expected time complexity is O(m+n) where m is the number of elements in ar1[] and n is the number of elements in ar2[].
+
+Examples:
+
+Input:  ar1[] = {2, 3, 7, 10, 12}, ar2[] = {1, 5, 7, 8}
+Output: 35
+35 is sum of 1 + 5 + 7 + 10 + 12.
+We start from first element of arr2 which is 1, then we
+move to 5, then 7.  From 7, we switch to ar1 (7 is common)
+and traverse 10 and 12.
+
+Input:  ar1[] = {10, 12}, ar2 = {5, 7, 9}
+Output: 22
+22 is sum of 10 and 12.
+Since there is no common element, we need to take all 
+elements from the array with more sum.
+
+Input:  ar1[] = {2, 3, 7, 10, 12, 15, 30, 34}
+        ar2[] = {1, 5, 7, 8, 10, 15, 16, 19}
+Output: 122
+122 is sum of 1, 5, 7, 8, 10, 12, 15, 30, 34
+*/
+
+
 // Utility function to find maximum of two integers
 int max(int x, int y) { return (x > y)? x : y; }
  
