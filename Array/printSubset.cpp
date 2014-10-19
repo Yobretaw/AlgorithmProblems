26fb8d28e06bd8b56461f1d1f781585f21313dfc
Yobretaw <youkeshen01@gmail.com>
2014-10-19 14:48:59 -0400

    DP/longestPalSubstring

--- Array/printSubset.cpp
+++ Array/printSubset.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 void printStringSubset(string s, int startIdx = 0, string curr = "");
+void printIntegerSubset(int n, vector<int>& curr, int startIdx);
 
 void printStringSubset(string s, int startIdx, string curr) {
   if(s.length() == 0) 
@@ -36,9 +37,30 @@ void printStringSubset(string s, int startIdx, string curr) {
   }
 }
 
+// print all subset of integers that are less than n
+void printIntegerSubset(int n, vector<int>& curr, int startIdx) {
+  for(int i = startIdx; i <= n; i++) {
+    curr.push_back(i);
+
+    for(int i = 0; i < curr.size(); i++) {
+      cout << curr[i];
+      if(i != curr.size() - 1) {
+        cout << " ";
+      }
+    }
+    cout << endl;
+
+    printIntegerSubset(n, curr, i + 1);
+    curr.pop_back();
+  }
+}
+
 
 int main(){
   string a = "abcde";
-  printStringSubset(a);
+  //printStringSubset(a);
+  int input = 15;
+  vector<int> curr;
+  printIntegerSubset(input, curr, 0);
   return 0;
 }
---
+++ DP/longestPalindromicSubstring.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <climits>
+using namespace std;
+
+
+string lps(string s) {
+  if(s.empty())
+    return "";
+
+  int n = s.length();
+  int begin = 0;
+  int maxlen = 1;
+  vector<vector<int> > table(n, vector<int>(n));
+
+  for(int i = 0; i < n; i++) {
+    table[i][i] = 1;
+  }
+
+  for(int i = 0; i < n - 1; i++) {
+    if(s[i] == s[i+1]) {
+      table[i][i+1] = 1;
+      begin = i;
+      maxlen = 2;
+    }
+  }
+
+
+  for(int len = 3; len <= n; len++) {
+    for(int i = 0; i < n - len + 1; ++i) {
+      int j = i + len - 1;
+      if(s[i] == s[j] && table[i+1][j-1] == 1) {
+        table[i][j] = 1;
+        begin = i;
+        maxlen = len;
+      }
+    }
+  }
+
+  return s.substr(begin, maxlen);
+}
+
+
+int main()
+{
+  string a = "abedebaba";
+  cout << lps(a) << endl;
+  return 0;
+}
