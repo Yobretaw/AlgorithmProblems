3917ff6c6e85dc120a0ea9229df002d0c37b41f0
Yobretaw <youkeshen01@gmail.com>
2014-11-17 16:14:13 -0500

    Nov_17_2014

---
+++ String/convertToReversePolishNotation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <stack>
+using namespace std;
+
+string convert(string s) {
+  stack<string> st;
+
+  for (int i = 0; i < s.length(); i++) {
+    if(s[i] == '(')
+      continue;
+    if(s[i] == ')') {
+      string post = st.top();
+      st.pop();
+      string op = st.top();
+      st.pop();
+      string pre = st.top();
+      st.pop();
+
+      st.push(pre + post + op);
+    } else {
+      string curr;
+      stringstream  ss;
+      ss << s[i];
+      ss >> curr;
+      st.push(curr);
+    }
+  }
+
+  return st.top();
+}
+
+int main() {
+  cout << convert("((a+t)*((b+(a+c))^(c+d)))") << endl;
+  return 0;
+}
