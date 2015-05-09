e02caa4e0034e60932a53d520053fab7a35bd657
Yobretaw <youkeshen@gmail.com>
2015-05-09 10:20:28 -0400

    .

--- Py_leetcode/69_sqrt.py
+++ Py_leetcode/69_sqrt.py
@@ -8,17 +8,29 @@ import math
     Compute and return the square root of x.
 """
 def my_sqrt(x):
-    if x < 2:
-        return x
+    start = 0
+    end = x
+    while start <= end:
+        mid = (start + end) / 2
+        if mid ** 2 <= x and (mid + 1) ** 2 > x:
+            return mid
+        elif mid ** 2 > x:
+            end = mid - 1
+        else:
+            start = mid + 1
 
-    res = 1
-    while res ** 2 < x:
-        m = 1
-        while (res + m << 1) ** 2 < x:
-            m <<= 1
-        print x, res, m
-        res += m
-    return res
 
-for i in range(1, 100):
-    print i, int(i ** 0.5), my_sqrt(i)
+"""
+    Assume the root is r. If i > r, then x/i < r < i, so j < i, so we should return i when j >= i.
+"""
+def my_sqrt_newton(x):
+    i = x
+    while i != 0:
+        j = (i + x / i) / 2
+        if j >= i:
+            return i
+        i = j
+    return i
+
+#for i in range(1, 100):
+#    print i, int(i ** 0.5), my_sqrt(i)
