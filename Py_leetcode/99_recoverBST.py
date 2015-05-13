86f473b181a2e9fdff9afb9786ab5969cb5bf4e3
Yobretaw <youkeshen@gmail.com>
2015-05-13 13:54:11 -0400

    .

--- Py_leetcode/99_recoverBST.py
+++ Py_leetcode/99_recoverBST.py
@@ -18,8 +18,11 @@ def recover_bst(root):
     if not root:
         return
 
-    data = {}
+    data = [None, None, False]
     recover_bst_help(root, data)
+    print data
+    if not data[2] and data[1]:
+        data[0].val, data[1].val = data[1].val, data[0].val
 
 
 def recover_bst_help(root, data):
@@ -27,19 +30,26 @@ def recover_bst_help(root, data):
         return
 
     recover_bst_help(root.left, data)
-    if not 'preNode' in data:
-        data['preNod'] = root
-    elif not 'changeNode' in data and root.val < data['prevNode'].val:
-        data['changeNode'] = data['prevNode']
-    elif 'changeNode' in data and root.val < data['prevNode'].val:
-        root.val, data['changeNode'].val = data['changeNode'].val, root.val
+    if not data[0]:
+        data[0] = root
+        recover_bst_help(root.right, data)
+    elif root.val < data[0].val:
+        if not data[1]:
+            data[1] = data[0]
+            data[0] = root
+            recover_bst_help(root.right, data)
+        else:
+            data[1].val, root.val = root.val, data[1].val
+            data[2] = True
     else:
-        data['prevNode'] = root
+        data[0] = root
         recover_bst_help(root.right, data)
 
-
-root = Node(0)
-root.left = Node(1)
+root = Node(0, Node(1))
+root = Node(5, Node(3, Node(1), Node(6)), Node(7, Node(4), Node(8)))
+root = Node(1, Node(2), Node(3))
+root = Node(1, None, Node(2, Node(3)))
 bst_print(root)
+print '-' * 100
 recover_bst(root)
 bst_print(root)
