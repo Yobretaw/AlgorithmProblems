0601f043c8d9a852c7fb16957c84d0c19253c58c
Yobretaw <youkeshen@gmail.com>
2015-05-22 18:37:27 -0400

    .

--- Py_leetcode/037_sudokuSolver.py
+++ Py_leetcode/037_sudokuSolver.py
@@ -3,6 +3,8 @@ import math
 import imp
 import time
 import copy
+#from sets import Set
+from statistics import mean, stdev
 
 is_valid_sudoku = imp.load_source('Node', './036_validSudoku.py').is_valid_sudoku
 
@@ -13,29 +15,41 @@ is_valid_sudoku = imp.load_source('Node', './036_validSudoku.py').is_valid_sudok
 
     You may assume that there will be only one unique solution.
 """
+FC_CHECKING = False
+#FC_CHECKING = True
 def sudoku_solver(board):
+    A = [[None for j in range(0, 9)] for i in range(0, 9)]
+    for a in A:
+        for i in range(0, 9):
+            a[i] = [False for i in range(1, 10)]
+
     for i in range(0, 9):
         board[i] = [c for c in board[i]]
-    sudoku_solver_help(board)
+    sudoku_solver_help(board, A)
     for i in range(0, 9):
         board[i] = ' | '.join(board[i])
 
 
-def sudoku_solver_help(board):
+def sudoku_solver_help(board, A):
     next_pos = find_next_pos(board)
     if not next_pos:
         return True
 
     i, j = next_pos
     for x in range(1, 10):
+        if FC_CHECKING:
+            if A[i][j][x - 1]: continue
         if is_valid_put(board, i, j, str(x)):
             board[i][j] = str(x)
-            if sudoku_solver_help(board):
+            if FC_CHECKING:
+                if A[i][j][x - 1]: continue
+            add_num_to_sets(A, i, j, x)
+            if sudoku_solver_help(board, A):
                 return True
             board[i][j] = '.'
-
+            if FC_CHECKING:
+                remove_num_from_sets(A, i, j, x)
     return False
-
 
 def find_next_pos(board):
     for i in range(0, 9):
@@ -61,6 +75,27 @@ def is_valid_put(board, row, col, val):
             if board[row + i][col + j] == val:
                 return False
     return True
+
+"""
+    Methods used in forward checking
+"""
+def remove_num_from_sets(A, row, col, val):
+    for i in range(0, 9):
+        A[row][i][val - 1] = False
+        A[col][i][val - 1] = False
+
+def add_num_to_sets(A, row, col, val):
+    for i in range(0, 9):
+        A[row][i][val - 1] = True
+        A[col][i][val - 1] = True
+
+
+"""
+    Optimization
+
+    1. Forward checking: maintain a 9x9 array A where A[i][j] is a set of numbers. Initally all sets have integers from 1 to 9.
+    2. CSP Heuristics(most contrained variable, most constraining variable and least constraining variable).
+"""
 
 
 #board = ["..9748...","7........",".2.1.9...","..7...24.",".64.1.59.",".98...3..","...8.3.2.","........6","...2759.."]
@@ -108,13 +143,17 @@ board4 = [
   "2.89....3",
   "........6",
 ]
-start_time = time.time()
+times = []
 for i in range(0, 50):
+  start_time = time.time()
   sudoku_solver(copy.deepcopy(board1))
   #for line in board:
   #    print ' -' + '--' * 17
   #    print '| ' + line + ' |'
   #print ' -' + '--' * 17
-  print i
-end_time= time.time()
-print end_time - start_time
+  print(i)
+  end_time= time.time()
+  times.append(end_time - start_time)
+
+print(mean(times))
+print(stdev(times))
