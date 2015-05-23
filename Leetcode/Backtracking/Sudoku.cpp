05a0a0ee240711b6e36d30e72095d806ac582875
Yobretaw <youkeshen@gmail.com>
2015-05-23 16:21:29 -0400

    sudoku

--- Leetcode/Backtracking/Sudoku.cpp
+++ Leetcode/Backtracking/Sudoku.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
 #include <stdio.h>
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <unordered_set>
 #include <utility>      // std::pair
 #include <ctime>
 #include <numeric>
+#include <queue>
 using namespace std;
 
 #define N 9
-#define ENABLE_FORWARD_CHECKING false
+#define ENABLE_FORWARD_CHECKING true
+#define MCDV false
+#define MCGV false
+#define LCGV false
 
 bool isSafe(int su[N][N], int row, int col, int num, int rows[N], int cols[N], int boxes[N]);
 bool findUassignedLocation(int su[N][N], int& row, int& col, vector<pair<int, int> >& emptys);
 bool solve(int su[N][N], int rows[N], int cols[N], int boxes[N], vector<pair<int, int> >& emptys);
 void printGrid(int grid[N][N]);
+bool isValidSudoku(int grid[N][N]);
 
+
+int GLOBAL_COUNT = 0;
 int IDX_TO_PRIME[9] = {2, 3, 5, 7, 11, 13 ,17, 19, 23};
 
 bool isSafe(int su[N][N], int row, int col, int num, int rows[N], int cols[N], int boxes[N]) {
+  if(!ENABLE_FORWARD_CHECKING) return true;
   num = IDX_TO_PRIME[num - 1];
   bool res = rows[row] % num != 0 && cols[col] % num != 0 && boxes[row - row % 3 + col / 3] % num != 0;
   return res;
@@ -26,8 +35,89 @@ bool isSafe(int su[N][N], int row, int col, int num, int rows[N], int cols[N], i
 
 bool findUassignedLocation(int su[N][N], int& row, int& col, vector<pair<int, int> >& emptys) {
   if(emptys.empty()) return false;
-  row = emptys[emptys.size() - 1].first;
-  col = emptys[emptys.size() - 1].second;
+
+  // Most Constrained Variable
+  if(MCDV) {
+    vector<int> rows(N);
+    vector<int> cols(N);
+    vector<int> boxes(N);
+
+    for(auto v : emptys) {
+      rows[v.first] += 1;
+      cols[v.second] += 1;
+      boxes[v.first - v.first % 3 + v.second / 3] += 1;
+    }
+
+    int max_val = INT_MIN;
+    for(int i = 0; i < N; ++i) {
+      for(int j = 0; j < N; ++j) {
+        int curr = rows[i] + cols[j] + boxes[i - i % 3 + j / 3];
+        if(su[i][j] == 0 && curr > max_val) {
+          row = i;
+          col = j;
+          max_val = curr;
+        }
+      }
+    }
+  } else if(MCGV) {
+    vector<int> rows(N);
+    vector<int> cols(N);
+    vector<int> boxes(N);
+
+    for(auto v : emptys) {
+      rows[v.first] += 1;
+      cols[v.second] += 1;
+      boxes[v.first - v.first % 3 + v.second / 3] += 1;
+    }
+
+    int max_count = INT_MAX;
+    for(int i = 0; i < N; ++i) {
+      for(int j = 0; j < N; ++j) {
+        int curr = rows[i] + cols[j] + boxes[i - i % 3 + j / 3];
+        if(su[i][j] == 0 && curr < max_count) {
+          row = i;
+          col = j;
+          max_count = curr;
+        }
+      }
+    }
+  } else if(LCGV) {
+    vector<int> rows(N);
+    vector<int> cols(N);
+    vector<int> boxes(N);
+
+    for(auto v : emptys) {
+      rows[v.first] += 1;
+      cols[v.second] += 1;
+      boxes[v.first - v.first % 3 + v.second / 3] += 1;
+    }
+
+    int max_count = INT_MAX;
+    for(int i = 0; i < N; ++i) {
+      for(int j = 0; j < N; ++j) {
+        int curr = rows[i] + cols[j] + boxes[i - i % 3 + j / 3];
+        if(su[i][j] == 0 && curr < max_count) {
+          row = i;
+          col = j;
+          max_count = curr;
+        }
+      }
+    }
+  }
+
+  if(MCDV || MCGV || LCGV) {
+    int idx = 0;
+    for(auto it = emptys.begin(); it != emptys.end(); ++it) {
+      if(it->first == row && it->second == col) {
+        emptys.erase(it);
+        emptys.push_back(pair<int, int>(row, col));
+        break;
+      }
+    }
+  } else {
+    row = emptys[emptys.size() - 1].first;
+    col = emptys[emptys.size() - 1].second;
+  }
   return true;
 }
 
@@ -35,7 +125,9 @@ bool solve(int su[N][N], int rows[N], int cols[N], int boxes[N], vector<pair<int
   int row, col;
 
   if(!findUassignedLocation(su, row, col, emptys)) {
-    return true;
+    if(GLOBAL_COUNT++ % 20000000 == 0)
+      cout << GLOBAL_COUNT << endl;
+    return ENABLE_FORWARD_CHECKING || isValidSudoku(su);
   }
 
   emptys.pop_back();
@@ -44,17 +136,21 @@ bool solve(int su[N][N], int rows[N], int cols[N], int boxes[N], vector<pair<int
       su[row][col] = i;
 
       int d = IDX_TO_PRIME[i - 1];
-      rows[row] *= d;
-      cols[col] *= d;
-      boxes[row - row % 3 + col / 3] *= d;
+      if(ENABLE_FORWARD_CHECKING) {
+        rows[row] *= d;
+        cols[col] *= d;
+        boxes[row - row % 3 + col / 3] *= d;
+      }
 
       if(solve(su, rows, cols, boxes, emptys)) {
         return true;
       }
 
-      rows[row] /= d;
-      cols[col] /= d;
-      boxes[row - row % 3 + col / 3] /= d;
+      if(ENABLE_FORWARD_CHECKING) {
+        rows[row] /= d;
+        cols[col] /= d;
+        boxes[row - row % 3 + col / 3] /= d;
+      }
       su[row][col] = 0;
     }
   }
@@ -69,6 +165,28 @@ void printGrid(int grid[N][N]) {
              printf("%2d", grid[row][col]);
         printf("\n");
     }
+}
+
+
+bool isValidSudoku(int grid[N][N]) {
+  int rows[N]; 
+  int cols[N]; 
+  int boxes[N]; 
+  for(int i = 0; i < N; ++i) {
+    rows[i] = cols[i] = boxes[i] = 1;
+  }
+  for(int i = 0; i < N; ++i) {
+    for(int j = 0; j < N; ++j) {
+      int d = IDX_TO_PRIME[grid[i][j] - 1];
+      if(rows[i] % d == 0 || cols[j] % d == 0 || boxes[i - i % 3 + j / 3] % d == 0){
+        return false;
+      }
+      rows[i] *= d;
+      cols[j] *= d;
+      boxes[i - i % 3 + j / 3] *= d;
+    }
+  }
+  return true;
 }
 
 int main(int argc, const char *argv[])
@@ -158,10 +276,11 @@ int main(int argc, const char *argv[])
       }
     }
     if(solve(grid, rows, cols, boxes, emptys)) {
-      //printGrid(grid);
       times[count++] = (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000);
+      //printGrid(grid);
+      //cout << isValidSudoku(grid) << endl;
     } else {
-      //cout << "NO SOLUTION FOUND!" << endl;
+      cout << "NO SOLUTION FOUND!" << endl;
     }
   }
 
