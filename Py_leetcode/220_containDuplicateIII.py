bdd77e5044839ce74a213aafb8d1ca61d9e503f4
Yobretaw <youkeshen@gmail.com>
2015-06-01 22:22:51 -0400

    Py_leetcode:220

--- Py_leetcode/220_containDuplicateIII.py
+++ Py_leetcode/220_containDuplicateIII.py
@@ -1,7 +1,8 @@
 import sys
 import math
 import imp
-from collections import defaultdict
+from collections import defaultdict, deque
+import heapq
 
 """
     Given an array of integers, find out whether there are two distinct indices i and j in
@@ -9,13 +10,30 @@ from collections import defaultdict
     between i and j is at most k.
 """
 def contain_nearby_almost_duplicate(nums, k, t):
-    nums = [(nums[i], i) for i in range(0, len(nums))]
-    nums.sort(key=lambda x: x[0])
+    if k < 1 or t < 0:
+        return False
 
-    print nums
-    for i in range(1, len(nums)):
-        if abs(nums[i][0] - nums[i - 1][0]) <= t and abs(nums[i][1] - nums[i - 1][1]) <= k:
+    m = {}
+    for i in range(0, len(nums)):
+        val = nums[i] + sys.maxint
+        bucket = val / (t + 1)
+        if bucket in m \
+                or (bucket - 1 in m and val - m[bucket - 1] <= t) \
+                or (bucket + 1 in m and m[bucket + 1] - val <= t):
             return True
+
+        if len(m) >= k:
+            last_bucket = (nums[i - k] + sys.maxint) / (t + 1)
+            m.pop(last_bucket, None)
+
+        m[bucket] = val
+
     return False
 
+
+#print contain_nearby_almost_duplicate([0], 0, 0)
+#print contain_nearby_almost_duplicate([1], 1, 1)
+#print contain_nearby_almost_duplicate([4, 2], 2, 1)
+#print contain_nearby_almost_duplicate([-1, -1], 1, -1)
+#print contain_nearby_almost_duplicate([-1, -1], 1, 0)
 print contain_nearby_almost_duplicate([0, 10, 22, 15, 0, 5, 22, 12, 1, 5], 3, 3)
