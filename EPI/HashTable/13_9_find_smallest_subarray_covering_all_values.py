4e6d97d293eb3bf9093df199e404b67d91115599
Yobretaw <youkeshen@gmail.com>
2015-10-18 16:45:11 -0700

    EPI - add 13_9

--- EPI/HashTable/13_9_find_smallest_subarray_covering_all_values.py
+++ EPI/HashTable/13_9_find_smallest_subarray_covering_all_values.py
@@ -9,22 +9,51 @@ from collections import defaultdict, Counter
     of the given array that "covers" the set, i.e., contains all strings in the
     set.
 """
-def find_smallest_subarray_convering_set(arr, s):
-    if len(arr) < len(s):
-        return -1, -1
+def find_smallest_subarray_convering_set(s, t):
+        if not t or not s or len(s) < len(t):
+            return ""
 
-    pos = {}
-    for i, v in enumerate(arr):
-        pos[v] = i
+        m, n = len(s), len(t)
+        allchar = Counter(t)
+        seen = defaultdict(int)
 
-    l, r = 0, len(arr) - 1
-    for ss in s:
-        if not ss in pos:
-            return -1, -1
-        if pos[ss] < l:
-            l = pos[ss]
-        elif pos[ss] > r:
-            r = pos[ss]
+        start, end = 0, sys.maxint
+        count = 0
 
-    return l, r
+        i = j = 0
+        while j < m:
+            while j < m and count < n:
+                if s[j] in allchar:
+                    if seen[s[j]] < allchar[s[j]]:
+                        count += 1
+                    seen[s[j]] += 1
+                j += 1
 
+            if count < n:
+                break
+
+            while i < j:
+                if not s[i] in allchar:
+                    i += 1
+                elif seen[s[i]] > allchar[s[i]]:
+                    seen[s[i]] -= 1
+                    i += 1
+                else:
+                    if (j - i) < (end - start):
+                        start, end = i, j
+                    break
+
+            if i < j:
+                seen[s[i]] -= 1
+                i += 1
+                count -= 1
+
+        if end == sys.maxint:
+            return ''
+        return s[start:end]
+
+
+if __name__ == '__main__':
+    print find_smallest_subarray_convering_set('ADOBECODEBANC','ABC')
+    print find_smallest_subarray_convering_set('aa','a')
+    print find_smallest_subarray_convering_set('aa','ab')
