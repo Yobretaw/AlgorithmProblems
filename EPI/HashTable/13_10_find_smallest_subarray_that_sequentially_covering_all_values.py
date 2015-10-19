1a376216e0c073efe7f3b8ac5d42ac3742d5dc66
Yobretaw <youkeshen@gmail.com>
2015-10-18 18:08:19 -0700

    EPI - add 13_10

--- EPI/HashTable/13_10_find_smallest_subarray_that_sequentially_covering_all_values.py
+++ EPI/HashTable/13_10_find_smallest_subarray_that_sequentially_covering_all_values.py
@@ -7,6 +7,49 @@ from collections import defaultdict, Counter
     Same as Leetcode: Minimum Window Substring, except for that the order of
     characters matters.
 """
+#def find_smallest_subarray_sequentially_covering_set(s, t):
+#    if not t or not s or len(s) < len(t):
+#        return ""
+
+#    pos = defaultdict(list)
+#    for i, c in enumerate(s):
+#        pos[c].append(i)
+
+
+#    start, end = 0, sys.maxint
+#    exit = False
+#    old_idx = {c:-1 for c in t}
+
+#    for first_idx, first_pos in enumerate(pos[t[0]]):
+#        prev_pos = first_pos
+#        old_idx[t[0]] = first_idx
+
+#        for i in range(1, len(t)):
+#            next_char = t[i]
+#            next_idx = old_idx[next_char] + 1
+#            arr = pos[next_char]
+
+#            while next_idx < len(arr) and arr[next_idx] < prev_pos:
+#                next_idx += 1
+
+#            if next_idx >= len(arr):
+#                exit = True
+#                break
+
+#            old_idx[next_char] = next_idx
+#            prev_pos = arr[next_idx]
+
+#        if exit:
+#            break
+
+#        if (prev_pos - first_pos) < (end - start):
+#            start, end = first_pos, prev_pos 
+#        if end - start + 1 == len(t):
+#            break
+
+#    return s[start:end + 1]
+
+
 def find_smallest_subarray_sequentially_covering_set(s, t):
     if not t or not s or len(s) < len(t):
         return ""
@@ -20,32 +63,25 @@ def find_smallest_subarray_sequentially_covering_set(s, t):
     exit = False
     old_idx = {c:-1 for c in t}
 
-    for first_idx, first_pos in enumerate(pos[t[0]]):
-        prev_pos = first_pos
-        old_idx[t[0]] = first_idx
+    prev_pos = 0
+    for i in range(0, len(t)):
+        next_char = t[i]
+        next_idx = old_idx[next_char] + 1
+        arr = pos[next_char]
 
-        for i in range(1, len(t)):
-            next_char = t[i]
-            next_idx = old_idx[next_char] + 1
-            arr = pos[next_char]
+        while next_idx < len(arr) and arr[next_idx] < prev_pos:
+            next_idx += 1
 
-            while next_idx < len(arr) and arr[next_idx] < prev_pos:
-                next_idx += 1
-
-            if next_idx >= len(arr):
-                exit = True
-                break
-
-            old_idx[next_char] = next_idx
-            prev_pos = arr[next_idx]
-
-        if exit:
+        if next_idx >= len(arr):
             break
 
-        if (prev_pos - first_pos) < (end - start):
-            start, end = first_pos, prev_pos 
-        if end - start + 1 == len(t):
-            break
+        old_idx[next_char] = next_idx
+        prev_pos = arr[next_idx]
+
+    if (prev_pos - first_pos) < (end - start):
+        start, end = first_pos, prev_pos 
+    if end - start + 1 == len(t):
+        break
 
     return s[start:end + 1]
 
