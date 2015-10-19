3135fc193f16b3cc7836ee1f5bc0f828c1dee62b
Yobretaw <youkeshen@gmail.com>
2015-10-18 17:58:05 -0700

    EPI - add 13_10

---
+++ EPI/HashTable/13_10_find_smallest_subarray_that_sequentially_covering_all_values.py
@@ -0,0 +1,58 @@
+import os
+import math
+import sys
+from collections import defaultdict, Counter
+
+"""
+    Same as Leetcode: Minimum Window Substring, except for that the order of
+    characters matters.
+"""
+def find_smallest_subarray_sequentially_covering_set(s, t):
+    if not t or not s or len(s) < len(t):
+        return ""
+
+    pos = defaultdict(list)
+    for i, c in enumerate(s):
+        pos[c].append(i)
+
+
+    start, end = 0, sys.maxint
+    exit = False
+    old_idx = {c:-1 for c in t}
+
+    for first_idx, first_pos in enumerate(pos[t[0]]):
+        prev_pos = first_pos
+        old_idx[t[0]] = first_idx
+
+        for i in range(1, len(t)):
+            next_char = t[i]
+            next_idx = old_idx[next_char] + 1
+            arr = pos[next_char]
+
+            while next_idx < len(arr) and arr[next_idx] < prev_pos:
+                next_idx += 1
+
+            if next_idx >= len(arr):
+                exit = True
+                break
+
+            old_idx[next_char] = next_idx
+            prev_pos = arr[next_idx]
+
+        if exit:
+            break
+
+        if (prev_pos - first_pos) < (end - start):
+            start, end = first_pos, prev_pos 
+        if end - start + 1 == len(t):
+            break
+
+    return s[start:end + 1]
+
+
+if __name__ == '__main__':
+    print find_smallest_subarray_sequentially_covering_set('ADOBECODEBANC','ABC')
+    print find_smallest_subarray_sequentially_covering_set('ADOBECODEBANC','BC')
+    print find_smallest_subarray_sequentially_covering_set('FACAI','AA')
+    print find_smallest_subarray_sequentially_covering_set('FACAI','A')
+    print find_smallest_subarray_sequentially_covering_set('FACAI','FACAI')
--- EPI/HashTable/13_9_find_smallest_subarray_covering_all_values.py
+++ EPI/HashTable/13_9_find_smallest_subarray_covering_all_values.py
@@ -8,49 +8,53 @@ from collections import defaultdict, Counter
     return the indices of the starting and ending index of a shortest subarray
     of the given array that "covers" the set, i.e., contains all strings in the
     set.
+
+    ====
+     
+    Same as Leetcode: Minimum Window Substring
 """
-def find_smallest_subarray_convering_set(s, t):
-        if not t or not s or len(s) < len(t):
-            return ""
+def find_smallest_subarray_covering_set(s, t):
+    if not t or not s or len(s) < len(t):
+        return ""
 
-        m, n = len(s), len(t)
-        allchar = Counter(t)
-        seen = defaultdict(int)
+    m, n = len(s), len(t)
+    allchar = Counter(t)
+    seen = defaultdict(int)
 
-        start, end = 0, sys.maxint
-        count = 0
+    start, end = 0, sys.maxint
+    count = 0
 
-        i = j = 0
-        while j < m:
-            while j < m and count < n:
-                if s[j] in allchar:
-                    if seen[s[j]] < allchar[s[j]]:
-                        count += 1
-                    seen[s[j]] += 1
-                j += 1
+    i = j = 0
+    while j < m:
+        while j < m and count < n:
+            if s[j] in allchar:
+                if seen[s[j]] < allchar[s[j]]:
+                    count += 1
+                seen[s[j]] += 1
+            j += 1
 
-            if count < n:
-                break
+        if count < n:
+            break
 
-            while i < j:
-                if not s[i] in allchar:
-                    i += 1
-                elif seen[s[i]] > allchar[s[i]]:
-                    seen[s[i]] -= 1
-                    i += 1
-                else:
-                    if (j - i) < (end - start):
-                        start, end = i, j
-                    break
-
-            if i < j:
+        while i < j:
+            if not s[i] in allchar:
+                i += 1
+            elif seen[s[i]] > allchar[s[i]]:
                 seen[s[i]] -= 1
                 i += 1
-                count -= 1
+            else:
+                if (j - i) < (end - start):
+                    start, end = i, j
+                break
 
-        if end == sys.maxint:
-            return ''
-        return s[start:end]
+        if i < j:
+            seen[s[i]] -= 1
+            i += 1
+            count -= 1
+
+    if end == sys.maxint:
+        return ''
+    return s[start:end]
 
 
 if __name__ == '__main__':
