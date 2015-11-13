cfb2608e1ea72970aa02f1d9cdb972f7a3d5be28
Yobretaw <youkeshen@gmail.com>
2015-11-13 10:40:44 -0800

    EPI - DP - 17_2 not done yet

---
+++ EPI/DynamicProgramming/17_2_edit_distance.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+
+def edit_distance(a, b):
+    m, n = len(a), len(b)
+    if not m or not n:
+        return m if not n else n
+
+    t = [[0 for i in range(n + 1)] for j in range(m + 1)]
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if a[i - 1] == b[j - 1]:
+                t[i][j] = t[i - 1][j - 1]
+            else:
+                t[i][j] = min(t[i - 1][j - 1], t[i - 1][j], t[i][j - 1]) + 1
+    return t[-1][-1]
+
+
+if __name__ == '__main__':
+    a = 'abc'
+    b = 'cba'
+    print edit_distance(a, b)
