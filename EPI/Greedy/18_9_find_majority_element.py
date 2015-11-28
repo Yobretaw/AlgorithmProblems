36c6650a8a2709b7e9eb60db1d086b2c6d338020
Yobretaw <youkeshen@gmail.com>
2015-11-28 11:20:16 -0800

    EPI - Greedy 18_9

--- EPI/Greedy/18_9_find_majority_element.py
+++ EPI/Greedy/18_9_find_majority_element.py
@@ -7,22 +7,22 @@ import math
     all elements.
 """
 def find_majority(A):
-        n = len(A)
-        if not n:
-            return None
+    n = len(A)
+    if not n:
+        return None
 
-        res = None
-        count = 0
-        for v in A:
-            if v == res:
-                count += 1
-            elif count > 0:
-                count -= 1
-            else:
-                res = v
-                count = 1
+    res = None
+    count = 0
+    for v in A:
+        if v == res:
+            count += 1
+        elif count > 0:
+            count -= 1
+        else:
+            res = v
+            count = 1
 
-        return res if count > 0 else None
+    return res if count > 0 else None
 
 
 if __name__ == '__main__':
