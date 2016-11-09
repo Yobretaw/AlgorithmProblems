696ee312038f4a2b068dbd008b188605eec0d8a3
Allen Shen <t-yoush@microsoft.com>
2016-11-08 16:05:44 -0800

    5.4

--- EPI/F#/EPI/PrimitiveTypes.fs
+++ EPI/F#/EPI/PrimitiveTypes.fs
@@ -47,7 +47,7 @@
     (*
         5.3 Bit Reversal
     *)
-    let _reverseBit num =
+    let reverseBit num =
         let rec helper num accu =
             match num with
             | 0 -> accu
@@ -55,14 +55,48 @@
         helper num 0
 
     type BitReverseTable() =
-        member private this.tbl = Map(seq{ for i = 0 to 255 do yield (i, (_reverseBit i)) })
+        member private this.Table = Map(seq{ for i = 0 to 255 do yield (i, (reverseBit i)) })
         member this.GetReversed num =
-            (this.tbl.[num >>> 24] <<< 24) +
-            (this.tbl.[(num >>> 16) &&& ((1 <<< 8) - 1)] <<< 16) +
-            (this.tbl.[(num >>> 8) &&& ((1 <<< 8) - 1)] <<< 8) +
-            this.tbl.[num &&& ((1 <<< 8) - 1)]
+            (this.Table.[num >>> 24] <<< 24) +
+            (this.Table.[(num >>> 16) &&& ((1 <<< 8) - 1)] <<< 16) +
+            (this.Table.[(num >>> 8) &&& ((1 <<< 8) - 1)] <<< 8) +
+            this.Table.[num &&& ((1 <<< 8) - 1)]
 
     do
         let t = BitReverseTable()
         for i = 0 to 10000 do
             printfn "%A : %A" i (t.GetReversed(i))
+
+    (*
+        5.4 Closest Integers with the Same Weight
+    *)
+    let ConvertToBinary (num: uint64) =
+        let rec helper (num: uint64) accu =
+            match num with
+            | 0UL -> accu
+            | _ -> helper (num >>> 1) ((num &&& 1UL).ToString() + accu)
+        helper num ""
+
+    let countOneBit (num: uint64) =
+        ConvertToBinary num |> String.filter (fun c -> c = '1') |> String.length
+        
+    let closestIntWIthSameWeight (num: uint64) =
+        let rec helper (v: uint64) i =
+            match v with
+            | 0UL
+            | 1UL when i = 63 -> failwith "All bits are 0 or 1"
+            | _ when (v &&& 1UL) <> ((v >>> 1) &&& 1UL) ->
+                ((v ^^^ 3UL) <<< i) + (num &&& ((1UL <<< i) - 1UL))
+            | _ -> helper (v >>> 1) (i + 1)
+        helper num 0
+
+    do
+        for i = 1 to 10000 do
+            printfn "%s" "--------------------------------"
+            printfn "%d" i
+            let res = closestIntWIthSameWeight (uint64 i)
+            printfn "%d" res
+            printfn "%A" (ConvertToBinary (uint64 i))
+            printfn "%A" (ConvertToBinary res)
+            printfn "%A" ((countOneBit (uint64 i)) = (countOneBit res))
+            printfn "%d" (if (uint64 i) > res then (uint64 i) - res else res - (uint64 i))
